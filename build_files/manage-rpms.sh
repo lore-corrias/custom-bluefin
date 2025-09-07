85903f8a7c57b8d397ab037b731f45a97b4b58e2
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-09-07 20:18:13 +0200

    Additions & Fixes - Added a .devcontainer.json for better management of source code - Added a sysctl configuration to allow unprivileged users to bind ports   >80 - Added a resolved.conf.d to automatically use DNSOverTLS with   Cloudflare (creds to https://github.com/astrovm/amyos) - Moved the docker-ce repo configuration under /etc

--- build_files/manage-rpms.sh
+++ build_files/manage-rpms.sh
@@ -12,8 +12,6 @@ rpm-ostree install \
 	https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-42.noarch.rpm \
 	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-42.noarch.rpm
 
-curl "https://download.docker.com/linux/fedora/docker-ce.repo" -o /etc/yum.repos.d/docker-ce.repo
-
 ## Install packages from the default list
 grep -v '^#' /ctx/packages/default.packages | xargs dnf5 install -y \
   && dnf5 clean all
